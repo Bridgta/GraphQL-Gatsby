1e2f398f50f76a988494a2d8e0dddd4b9b172111
Bridget Asser <bridgetasser@gmail.com>
2020-12-28 21:39:48 -0500

    generating slugs for md files

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            This is the writing template
+        </Layout>
+    )
+}
+
+export default Blog
